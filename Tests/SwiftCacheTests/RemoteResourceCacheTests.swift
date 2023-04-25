690ccd08e11bc92ba9a7495bd66dca345828f8e9
Óscar Morales Vivó <oscarmv@mac.com>
2023-04-24 22:27:27 -0700

    WiP: Testing repair.

--- Tests/SwiftCacheTests/RemoteResourceCacheTests.swift
+++ Tests/SwiftCacheTests/RemoteResourceCacheTests.swift
@@ -41,169 +41,206 @@ final class SwiftCacheTests: XCTestCase {
 
     private static let dummyURL = URL(string: "https://zombo.com/")!
 
-    private typealias TestImageCache = RemoteImageCache<UniqueFileNameResourceIdentifier>
+    private typealias MockImageStorage = MockStorage<UIImage, URL>
+
+    private typealias MockNetworkStorage = MockStorage<Data, URL>
+
+    private typealias MockLocalStorage = MockStorage<Data, String>
+
+    private struct MockImageCache {
+        var cache: any Cache<UIImage, URL>
+
+        var inMemoryStorage: MockImageStorage
+
+        var localStorage: MockLocalStorage
+
+        var networkStorage: MockNetworkStorage
+    }
+
+    private static func buildImageCache() -> MockImageCache {
+        let networkStorage = MockNetworkStorage()
+        let networkCache = BackstopStorageCache(storage: networkStorage)
+
+        let localStorage = MockLocalStorage()
+        let localCache = TemporaryStorageCache(next: networkCache, storage: localStorage, idConverter: { url in
+            url.lastPathComponent
+        })
+
+        let inMemoryStorage = MockImageStorage()
+        let inMemoryCache = TemporaryStorageCache(next: localCache, storage: inMemoryStorage) { data in
+            UIImage(data: data)!
+        }
+
+        return .init(
+            cache: inMemoryCache,
+            inMemoryStorage: inMemoryStorage,
+            localStorage: localStorage,
+            networkStorage: networkStorage
+        )
+    }
 
     // If the data is found locally, we return it and don't do anything else weird.
     func testLocallyStoredImageDataHappyPath() async throws {
+        let imageCache = Self.buildImageCache()
         let localDataExpectation = expectation(description: "Grabbing local data")
-        let mockImageDataProvider = MockResourceDataProvider { _ in
-            XCTFail("Unexpected call to remoteData(imageURL:)")
-            return Self.sampleImageData
-        } localDataOverride: { localIdentifier in
+        imageCache.localStorage.storedValueForOverride = { fileName in
             localDataExpectation.fulfill()
-            XCTAssertEqual(localIdentifier, Self.dummyURL.lastPathComponent)
+            XCTAssertEqual(fileName, Self.dummyURL.lastPathComponent)
             return Self.sampleImageData
-        } storeLocallyOverride: { _, _ in
-            XCTFail("Unexpected call to storeLocally(data:imageURL:)")
         }
 
-        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
+        let inMemoryExpectation = expectation(description: "No in memory data found")
+        imageCache.inMemoryStorage.storedValueForOverride = { url in
+            XCTAssertEqual(url, Self.dummyURL)
+            inMemoryExpectation.fulfill()
+            return nil
+        }
 
-        let image = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
+        let image = try await imageCache.cache.fetchResourceWith(identifier: Self.dummyURL)
 
-        await fulfillment(of: [localDataExpectation])
+        await fulfillment(of: [localDataExpectation, inMemoryExpectation])
 
         // Looping image -> data -> image -> data doesn't usually result in equal data or equal images as some config
         // data gets lost, but at least we can check pixel size.
-        XCTAssertEqual(image.cgImage?.width, Self.sampleImage.cgImage?.width)
-        XCTAssertEqual(image.cgImage?.height, Self.sampleImage.cgImage?.height)
+        XCTAssertEqual(image?.cgImage?.width, Self.sampleImage.cgImage?.width)
+        XCTAssertEqual(image?.cgImage?.height, Self.sampleImage.cgImage?.height)
     }
 
     // If the data is not local, we get remote and store.
-    func testRemotelyStoredImageDataHappyPath() async throws {
-        struct DummyError: Error {}
-
-        let localDataExpectation = expectation(description: "Attempting to grab local data")
-        let remoteDataExpectation = expectation(description: "Grabbing remote data")
-        let localStoreExpectation = expectation(description: "Storing remote data locally")
-        let mockImageDataProvider = MockResourceDataProvider { _ in
-            remoteDataExpectation.fulfill()
-            return Self.sampleImageData
-        } localDataOverride: { _ in
-            localDataExpectation.fulfill()
-            throw DummyError()
-        } storeLocallyOverride: { _, _ in
-            localStoreExpectation.fulfill()
-        }
-
-        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
-
-        let image = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
-
-        await fulfillment(of: [localDataExpectation, remoteDataExpectation, localStoreExpectation])
-
-        // Looping image -> data -> image -> data doesn't usually result in equal data or equal images as some config
-        // data gets lost, but at least we can check pixel size.
-        XCTAssertEqual(image.cgImage?.width, Self.sampleImage.cgImage?.width)
-        XCTAssertEqual(image.cgImage?.height, Self.sampleImage.cgImage?.height)
-    }
-
-    // If the local data is bad we recover by grabbing remote again.
-    func testLocalDataIsBad() async throws {
-        struct DummyError: Error {}
-
-        let localDataExpectation = expectation(description: "Attempting to grab local data")
-        let remoteDataExpectation = expectation(description: "Grabbing remote data")
-        let localStoreExpectation = expectation(description: "Storing remote data locally")
-        let mockImageDataProvider = MockResourceDataProvider { _ in
-            remoteDataExpectation.fulfill()
-            return Self.sampleImageData
-        } localDataOverride: { _ in
-            localDataExpectation.fulfill()
-            return Self.badImageData
-        } storeLocallyOverride: { _, _ in
-            localStoreExpectation.fulfill()
-        }
-
-        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
-
-        let image = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
-
-        await fulfillment(of: [localDataExpectation, remoteDataExpectation, localStoreExpectation])
-
-        // Looping image -> data -> image -> data doesn't usually result in equal data or equal images as some config
-        // data gets lost, but at least we can check pixel size.
-        XCTAssertEqual(image.cgImage?.width, Self.sampleImage.cgImage?.width)
-        XCTAssertEqual(image.cgImage?.height, Self.sampleImage.cgImage?.height)
-    }
-
-    // If the remote data is bad we throw.
-    func testRemoteDataIsBad() async throws {
-        struct LocalDummyError: Error {}
-        struct RemoteDummyError: Error {}
-
-        let localDataExpectation = expectation(description: "Attempting to grab local data")
-        let remoteDataExpectation = expectation(description: "Grabbing remote data")
-        let mockImageDataProvider = MockResourceDataProvider { _ in
-            remoteDataExpectation.fulfill()
-            return Self.badImageData
-        } localDataOverride: { _ in
-            localDataExpectation.fulfill()
-            throw LocalDummyError()
-        } storeLocallyOverride: { _, _ in
-            XCTFail("We shouldn't have made it here")
-        }
-
-        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
-
-        do {
-            _ = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
-            XCTFail("Shouldn't have made it here")
-        } catch {
-            XCTAssert(error is TestImageCache.UnableToDecodeImageFromData)
-        }
-
-        await fulfillment(of: [localDataExpectation, remoteDataExpectation])
-    }
-
-    // Tests that after an error we clean things up well enough to run a successful happy path if it's there.
-    func testRemoteDataIsBadButRetryWorks() async throws {
-        struct LocalDummyError: Error {}
-        struct RemoteDummyError: Error {}
-
-        let localDataExpectation = expectation(description: "Attempting to grab local data")
-        let remoteFailureDataExpectation = expectation(description: "Grabbing remote data and failing")
-        let mockImageDataProvider = MockResourceDataProvider { _ in
-            remoteFailureDataExpectation.fulfill()
-            return Self.badImageData
-        } localDataOverride: { _ in
-            localDataExpectation.fulfill()
-            throw LocalDummyError()
-        } storeLocallyOverride: { _, _ in
-            XCTFail("We shouldn't have made it here")
-        }
-
-        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
-
-        do {
-            _ = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
-            XCTFail("Shouldn't have made it here")
-        } catch {
-            XCTAssert(error is TestImageCache.UnableToDecodeImageFromData)
-        }
-
-        await fulfillment(of: [localDataExpectation, remoteFailureDataExpectation])
-
-        let remoteSuccessDataExpectation = expectation(description: "Grabbing remote data and succeeding")
-        mockImageDataProvider.remoteDataOverride = { _ in
-            remoteSuccessDataExpectation.fulfill()
-            return Self.sampleImageData
-        }
-
-        let localDataExpectationRetry = expectation(description: "Attempting to grab local data _again_")
-        mockImageDataProvider.localDataOverride = { _ in
-            localDataExpectationRetry.fulfill()
-            throw LocalDummyError()
-        }
-
-        let storeLocallyExpectation = expectation(description: "Storing this time")
-        mockImageDataProvider.storeLocallyOverride = { _, _ in
-            storeLocallyExpectation.fulfill()
-        }
-
-        _ = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
-
-        await fulfillment(of: [remoteSuccessDataExpectation, localDataExpectationRetry, storeLocallyExpectation])
-    }
+//    func testRemotelyStoredImageDataHappyPath() async throws {
+//        struct DummyError: Error {}
+//
+//        let localDataExpectation = expectation(description: "Attempting to grab local data")
+//        let remoteDataExpectation = expectation(description: "Grabbing remote data")
+//        let localStoreExpectation = expectation(description: "Storing remote data locally")
+//        let mockImageDataProvider = MockResourceDataProvider { _ in
+//            remoteDataExpectation.fulfill()
+//            return Self.sampleImageData
+//        } localDataOverride: { _ in
+//            localDataExpectation.fulfill()
+//            throw DummyError()
+//        } storeLocallyOverride: { _, _ in
+//            localStoreExpectation.fulfill()
+//        }
+//
+//        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
+//
+//        let image = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
+//
+//        await fulfillment(of: [localDataExpectation, remoteDataExpectation, localStoreExpectation])
+//
+//        // Looping image -> data -> image -> data doesn't usually result in equal data or equal images as some config
+//        // data gets lost, but at least we can check pixel size.
+//        XCTAssertEqual(image.cgImage?.width, Self.sampleImage.cgImage?.width)
+//        XCTAssertEqual(image.cgImage?.height, Self.sampleImage.cgImage?.height)
+//    }
+//
+//    // If the local data is bad we recover by grabbing remote again.
+//    func testLocalDataIsBad() async throws {
+//        struct DummyError: Error {}
+//
+//        let localDataExpectation = expectation(description: "Attempting to grab local data")
+//        let remoteDataExpectation = expectation(description: "Grabbing remote data")
+//        let localStoreExpectation = expectation(description: "Storing remote data locally")
+//        let mockImageDataProvider = MockResourceDataProvider { _ in
+//            remoteDataExpectation.fulfill()
+//            return Self.sampleImageData
+//        } localDataOverride: { _ in
+//            localDataExpectation.fulfill()
+//            return Self.badImageData
+//        } storeLocallyOverride: { _, _ in
+//            localStoreExpectation.fulfill()
+//        }
+//
+//        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
+//
+//        let image = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
+//
+//        await fulfillment(of: [localDataExpectation, remoteDataExpectation, localStoreExpectation])
+//
+//        // Looping image -> data -> image -> data doesn't usually result in equal data or equal images as some config
+//        // data gets lost, but at least we can check pixel size.
+//        XCTAssertEqual(image.cgImage?.width, Self.sampleImage.cgImage?.width)
+//        XCTAssertEqual(image.cgImage?.height, Self.sampleImage.cgImage?.height)
+//    }
+//
+//    // If the remote data is bad we throw.
+//    func testRemoteDataIsBad() async throws {
+//        struct LocalDummyError: Error {}
+//        struct RemoteDummyError: Error {}
+//
+//        let localDataExpectation = expectation(description: "Attempting to grab local data")
+//        let remoteDataExpectation = expectation(description: "Grabbing remote data")
+//        let mockImageDataProvider = MockResourceDataProvider { _ in
+//            remoteDataExpectation.fulfill()
+//            return Self.badImageData
+//        } localDataOverride: { _ in
+//            localDataExpectation.fulfill()
+//            throw LocalDummyError()
+//        } storeLocallyOverride: { _, _ in
+//            XCTFail("We shouldn't have made it here")
+//        }
+//
+//        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
+//
+//        do {
+//            _ = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
+//            XCTFail("Shouldn't have made it here")
+//        } catch {
+//            XCTAssert(error is TestImageCache.UnableToDecodeImageFromData)
+//        }
+//
+//        await fulfillment(of: [localDataExpectation, remoteDataExpectation])
+//    }
+//
+//    // Tests that after an error we clean things up well enough to run a successful happy path if it's there.
+//    func testRemoteDataIsBadButRetryWorks() async throws {
+//        struct LocalDummyError: Error {}
+//        struct RemoteDummyError: Error {}
+//
+//        let localDataExpectation = expectation(description: "Attempting to grab local data")
+//        let remoteFailureDataExpectation = expectation(description: "Grabbing remote data and failing")
+//        let mockImageDataProvider = MockResourceDataProvider { _ in
+//            remoteFailureDataExpectation.fulfill()
+//            return Self.badImageData
+//        } localDataOverride: { _ in
+//            localDataExpectation.fulfill()
+//            throw LocalDummyError()
+//        } storeLocallyOverride: { _, _ in
+//            XCTFail("We shouldn't have made it here")
+//        }
+//
+//        let imageCache = TestImageCache(imageDataProvider: mockImageDataProvider)
+//
+//        do {
+//            _ = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
+//            XCTFail("Shouldn't have made it here")
+//        } catch {
+//            XCTAssert(error is TestImageCache.UnableToDecodeImageFromData)
+//        }
+//
+//        await fulfillment(of: [localDataExpectation, remoteFailureDataExpectation])
+//
+//        let remoteSuccessDataExpectation = expectation(description: "Grabbing remote data and succeeding")
+//        mockImageDataProvider.remoteDataOverride = { _ in
+//            remoteSuccessDataExpectation.fulfill()
+//            return Self.sampleImageData
+//        }
+//
+//        let localDataExpectationRetry = expectation(description: "Attempting to grab local data _again_")
+//        mockImageDataProvider.localDataOverride = { _ in
+//            localDataExpectationRetry.fulfill()
+//            throw LocalDummyError()
+//        }
+//
+//        let storeLocallyExpectation = expectation(description: "Storing this time")
+//        mockImageDataProvider.storeLocallyOverride = { _, _ in
+//            storeLocallyExpectation.fulfill()
+//        }
+//
+//        _ = try await imageCache.fetchResourceWith(identifier: .init(Self.dummyURL)).value
+//
+//        await fulfillment(of: [remoteSuccessDataExpectation, localDataExpectationRetry, storeLocallyExpectation])
+//    }
 }
 #endif
