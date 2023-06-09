a47585633f53db3a55deb8afcdd3d868a44b08ae
Óscar Morales Vivó <oscarmv@mac.com>
2023-06-08 18:10:09 -0700

    Use older graphics API so we can run tests on watchOS.

--- Tests/SwiftCacheTests/CacheChainTests.swift
+++ Tests/SwiftCacheTests/CacheChainTests.swift
@@ -5,33 +5,38 @@
 //  Created by Óscar Morales Vivó on 4/11/23.
 //
 
-#if canImport(UIKit) && !os(watchOS)
+#if canImport(UIKit)
 import SwiftCache
 import XCTest
 
 final class CacheChainTests: XCTestCase {
     private static let sampleImage: UIImage = {
         let imageSize = CGSize(width: 256.0, height: 256.0)
-        let imageRenderer = UIGraphicsImageRenderer(size: imageSize)
-        return imageRenderer.image { context in
-            UIColor.yellow.setFill()
-            context.fill(.init(origin: .zero, size: imageSize))
-            UIColor.blue.setFill()
-            UIBezierPath(ovalIn: .init(
-                x: imageSize.width / 8.0,
-                y: imageSize.height / 8.0,
-                width: imageSize.width / 4.0,
-                height: imageSize.height / 4.0
-            )
-            ).fill()
-            UIBezierPath(ovalIn: .init(
-                x: (imageSize.width * 5.0) / 8.0,
-                y: (imageSize.height * 5.0) / 8.0,
-                width: imageSize.width / 4.0,
-                height: imageSize.height / 4.0
-            )
-            ).fill()
+        UIGraphicsBeginImageContext(imageSize)
+        defer {
+            UIGraphicsEndImageContext()
         }
+
+        let context = UIGraphicsGetCurrentContext()!
+        UIColor.yellow.setFill()
+        context.fill(.init(origin: .zero, size: imageSize))
+        UIColor.blue.setFill()
+        UIBezierPath(ovalIn: .init(
+            x: imageSize.width / 8.0,
+            y: imageSize.height / 8.0,
+            width: imageSize.width / 4.0,
+            height: imageSize.height / 4.0
+        )
+        ).fill()
+        UIBezierPath(ovalIn: .init(
+            x: (imageSize.width * 5.0) / 8.0,
+            y: (imageSize.height * 5.0) / 8.0,
+            width: imageSize.width / 4.0,
+            height: imageSize.height / 4.0
+        )
+        ).fill()
+
+        return UIGraphicsGetImageFromCurrentImageContext()!
     }()
 
     private static let sampleImageData: Data = sampleImage.pngData()!
